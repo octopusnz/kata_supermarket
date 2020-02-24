332941365a7cfb2a0ebfc9b898f745cc20b51793
Jacob Doherty <jacobd@debian.theflat.gen.nz>
2020-02-24 21:17:14 +1300

    la

---
+++ spec/supermarket_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'supermarket'
+
+RSpec.describe TestSupermarket do
+  it 'has a list of groceries' do
+    TestSupermarket = Supermarket.new
+      expect TestSupermarket = 'milk'
+  end
+end
